002cb33c9446b194ce966b73ca1351934f02a0d8
Shilpa <shilpa@teg.london>
2025-02-28 18:28:21 +0530

    added desktop.py

---
+++ tldraw_whiteboard/config/desktop.py
@@ -0,0 +1,12 @@
+def get_data():
+    return [
+        {
+            "module_name": "tldraw_blackboard",
+            "category": "Modules",
+            "label": _("Tldraw_Blackboard"),
+            "icon": "octicon octicon-file-directory",
+            "type": "module",
+            "color": "blue",
+            "hidden": 0
+        }
+    ]
